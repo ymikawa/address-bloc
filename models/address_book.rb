f1c4aad3ee9a6904895d93736be52643949bf47b
ymikawa <yukimikawa@gmail.com>
2016-01-14 02:47:50 -0800

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,4 +21,16 @@ class AddressBook
        @entries.insert(index, Entry.new(name, phone_number, email))
      end
 
+     def remove_entry(name, phone_number, email)
+       delete_entry = []
+
+       @entries.each { |entry|
+         if name == entry.name && phone_number == entry.phone_number && email == entry.email
+           delete_entry = entry
+         end
+       }
+
+       @entries.delete(delete_entry)
+     end
+
 end
